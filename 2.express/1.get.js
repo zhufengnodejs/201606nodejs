d966e67fbd1ac7c08f92fce3994e9ec5de4e37f8
zhufengnodejs <zhufengnodejs@126.com>
2016-07-23 11:18:01 +0800

    路由配置

--- 2.express/1.get.js
+++ 2.express/1.get.js
@@ -3,14 +3,21 @@
  */
 var express = require('./express');
 var app = express();
+
 //路由配置
 // get 请求的方式  /hello是路径
-app.get('/hello',function(req,res){
+//表示不考虑方法名，只考虑路径
+app.all('/hello',function(req,res){
   res.end('hello');
 });
 
 app.get('/world',function(req,res){
     res.end('world');
+});
+
+//能匹配所有的路径
+app.get('*',function(req,res){
+    res.end('Not Found');
 });
 app.listen(3000);
 
--- 2.express/express/index.js
+++ 2.express/express/index.js
@@ -3,19 +3,28 @@ var app = function(req,res){
   var pathname = require('url').parse(req.url,true).pathname;//获取请求的路径
   for(var i=0;i<app.router.length;i++){
       var route = app.router[i];
-      if(method == route.method && pathname == route.path){
+      //方法名相同，路径相同或者路由配置配置为*
+      if(method == route.method && (pathname == route.path || route.path=='*')){
           return route.fn(req,res);
       }
   }
-  res.end(`Cannot ${method} ${pathname}`)
+  res.end(`Cannot ${method} ${pathname}`);
 }
 function express(){
     app.router = [];//里面存放着所有的路由配置项
     return app;
 }
-//这才是真正请求监听函数 配置路由
-app.get = function(path,fn){
-    app.router.push({method:'get',path:path,fn:fn});
+//循环所有的方法，给app增加这些方法名同名的属性
+['get','post','put','delete','option','patch'].forEach(function(method){
+    app[method] = function(path,fn){
+        app.router.push({method:method,path:path,fn:fn});
+    }
+});
+//配置所有的方法
+app.all = function(path,fn){
+    ['get','post','put','delete','option','patch'].forEach(function(method){//增加路由配置
+        app.router.push({method:method,path:path,fn:fn});
+    });
 }
 
 app.listen = function(){
