9c60e83f74c7f5ea597336c10c1a7943a61c4521
zhufengnodejs <zhufengnodejs@126.com>
2016-07-30 12:54:15 +0800

    mongoose

--- 5.mongoose/2.find.js
+++ 5.mongoose/2.find.js
@@ -20,7 +20,9 @@ var PersonSchema = new mongoose.Schema({
     birthday:{type:Date,required:true,default:Date.now}
     //如果你想固定下来集合的名称，需要加这样一个参数
 });
-
+PersonSchema.statics.findId = function(_id,cb){
+    this.findOne({_id:_id},cb);
+}
 var PersonModel = mongoose.model('Person',PersonSchema);
 
 var persons = [];
@@ -43,5 +45,24 @@ var cb = function(err,result){
 //PersonModel.update({name:new RegExp(/^zfpx/)},{$inc:{age:3}},{multi:true},cb);
 //默认会删除掉全部匹配的文档
 //如果希望最多只删除匹配到的一条
-PersonModel.remove({name:new RegExp(/zfpx/)},cb)
+//PersonModel.remove({name:new RegExp(/zfpx/)},cb)
 
+//PersonModel.find({},{age:1,_id:0},cb);
+
+
+/*
+PersonModel.find({_id:'579c2d8593f14c601a33b097'},function(err,result){
+    cb(err,result[0]);
+});
+*/
+/*
+
+PersonModel.findId('579c2d8593f14c601a33b097',cb);
+PersonModel.update({age:1},{$set:{home:'beijing'}},cb);
+PersonModel.find({home:{$exists:true}},cb);
+*/
+
+//PersonModel.find({},{age:1,name:1},{limit1:2,skip:3,sort:{age:-1}},cb);
+
+//查询所有的数据
+PersonModel.find({}).sort({age:1}).skip(3).limit(3).exec(cb);
